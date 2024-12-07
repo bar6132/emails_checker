335bb79418ae73f53596385fecee0b985a28978c
bar6132 <barbar6132@gmail.com>
2024-12-07 16:26:07 +0200

    Remove ignored files from tracking

--- mail_reminder_service.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"C:\Users\Bar_PC\Desktop\Project\Python-Service\.venv\Scripts\pythonw.exe" "C:\Users\Bar_PC\Desktop\Project\Python-Service\reminder_service.py"
-exit
--- run_reminder_service.vbs
+++
@@ -1,2 +0,0 @@
-Set WshShell = CreateObject("WScript.Shell")
-WshShell.Run "C:\Users\Bar_PC\Desktop\Project\Python-Service\mail_reminder_service.bat", 0
